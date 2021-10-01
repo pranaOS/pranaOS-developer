420fcc79728a79050296958ede67ef6273c763d8
krishpranav <krisna.pranav@gmail.com>
2021-10-01 17:57:38 +0530

    create command line application ruby

--- create-commandline-pranaOS/createcommandline.rb
+++ create-commandline-pranaOS/createcommandline.rb
@@ -1,11 +1,17 @@
-class CreateCommandLine
-	def write
-	end
+#!/usr/bin/env ruby
+
+class CreateCommandLineApplication
 
 	def read
 	end
+
+	def write
+	end
+
 end
 
 def main
-	
+	createcliapp = CreateCommandLineApplication.new()
 end
+
+main
